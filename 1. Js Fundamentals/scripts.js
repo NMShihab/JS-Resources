57ecb3a33416457fbcc740a2bde7e855b72f411b
NMShihab <nmshihabislam@gmail.com>
2021-03-30 22:27:41 +0600

    js fundamental part 1

---
+++ 1. Js Fundamentals/scripts.js
@@ -0,0 +1,15 @@
+let a = 5;
+if (a === 5) alert("Welcome to Js fundamentals!");
+
+// Show anything in console
+console.log(20 + 88 - 50);
+
+// Values and Variables
+let country = "Bangladesh";
+let continent = "Asia";
+let population = 170.06;
+
+// Show these variables in console
+console.log(country);
+console.log(continent);
+console.log(population);
