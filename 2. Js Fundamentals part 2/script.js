fb501fe9c49aa8566170a8767bdedd2fbbbc7e50
NMShihab <nmshihabislam@gmail.com>
2021-04-02 12:17:09 +0600

    loop

--- 2. Js Fundamentals part 2/script.js
+++ 2. Js Fundamentals part 2/script.js
@@ -138,6 +138,7 @@ console.log(mark.clacBMI(), john.clacBMI());
 
 // "John's BMI (28.3) is higher than Mark's (23.9)!";
 
+// Way 1
 console.log(
   `${
     mark.clacBMI() > john.clacBMI()
@@ -145,3 +146,74 @@ console.log(
       : `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
   }`
 );
+
+// Way 2
+mark.clacBMI() > john.clacBMI()
+  ? console.log(
+      `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+    )
+  : console.log(
+      `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+    );
+
+/* 
+Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays �
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to
+solve it:
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+GOOD LUCK �
+*/
+
+const billsArray = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tipsArray = [];
+const totalsArray = [];
+
+// const calcTip = (bill) => {
+//   if (bill >= 50 && bill <= 300) {
+//     return bill * 0.15;
+//   }
+
+//   return bill * 0.2;
+// };
+
+for (let i = 0; i < billsArray.length; i++) {
+  const tip = calcTip(billsArray[i]);
+  tipsArray.push(tip);
+  totalsArray.push(tip + billsArray[i]);
+}
+
+console.log(tipsArray, totalsArray);
+
+// Calculate avarage of an array
+function calcAvg(arr) {
+  let sum = 0;
+  let avarage = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  avarage = sum / arr.length;
+  return avarage;
+}
+
+const avgTotals = calcAvg(totalsArray);
+console.log(avgTotals);
