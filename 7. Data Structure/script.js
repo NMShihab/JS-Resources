b0a4984ed81c83ea9bc3482880322b48cf273a45
NMShihab <nmshihabislam@gmail.com>
2021-04-14 12:19:32 +0600

    Data structure ch-4 my solution

--- 7. Data Structure/script.js
+++ 7. Data Structure/script.js
@@ -326,3 +326,50 @@ for (const [keys, eventsHappen] of gameEvents) {
     ? console.log(`[FIRST HALF] ${keys} : ${eventsHappen}`)
     : console.log(`[SECOND HALF] ${keys} : ${eventsHappen}`);
 }
+
+/* 
+Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+ calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+Hints:
+§ Remember which character defines a new line in the textarea �
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working �
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK � */
+console.log("-------------Chalange 4--------------");
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const textArr = text.split("\n"); // Split text according to new line
+
+  for (let i = 0; i < textArr.length; i++) {
+    const txt = textArr[i];
+    const lowerText = txt.toLowerCase(); // convert text into lower
+    const splitText = lowerText.split("_"); // Split where find "_"
+    const secondText = splitText[1]; // asign second split word
+    const finalText =
+      splitText[0] + secondText[0].toUpperCase() + secondText.slice(1); // Create string
+    console.log(`${finalText} ${"✅".repeat(i + 1)}`); // log result and repeat "✅" according to its Arrayindex +1
+  }
+});
