997c2b7be24676e20bfdcd373fec630c2f7dbbf7
NMShihab <nmshihabislam@gmail.com>
2021-04-27 15:14:04 +0600

    chalange-2 async

--- 11. Asynchronous/script.js
+++ 11. Asynchronous/script.js
@@ -85,7 +85,7 @@ const getCountryData = function (country) {
     })
     .then((response) => {
       if (!response.ok) throw new Error(`Country not found ${response.status}`);
-      response.json();
+      return response.json();
     }) // Convert to json
     .then((data) => renderCountry(data, "neighbour")) // Show neighbour country
     .catch((err) => {
@@ -97,9 +97,9 @@ const getCountryData = function (country) {
     });
 };
 
-btn.addEventListener("click", function () {
-  getCountryData("bangladesh");
-});
+// btn.addEventListener("click", function () {
+//   getCountryData("bangladesh");
+// });
 
 /*
 Coding Challenge #1
@@ -141,6 +141,7 @@ Test data:
 GOOD LUCK �
 */
 
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then((response) => {
@@ -163,3 +164,137 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+// Geo location acces
+// navigator.geolocation.getCurrentPosition(
+//   (position) => console.log(position),
+//   (err) => console.error(err)
+// );
+
+const getCurrentLocation = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(position),
+      (err) => reject(err)
+    );
+  });
+};
+
+const whereAmI = function () {
+  getCurrentLocation()
+    .then((pos) => {
+      const { latitude, longitude } = pos.coords;
+      return fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json`);
+    })
+
+    .then((response) => {
+      if (!response.ok) throw new Error(`Not found try again`);
+      return response.json();
+    })
+    .then((data) => {
+      // getCountryData(data.country);
+      console.log(`You are in ${data.region}, ${data.country}`);
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then((response) => {
+      if (!response.ok) throw new Error(` Country is not found`);
+      return response.json();
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => console.log(err));
+};
+
+btn.addEventListener("click", whereAmI);
+
+/*
+Coding Challenge #2
+For this challenge you will actually have to watch the video! Then, build the image
+loading functionality that I just showed you on the screen.
+Your tasks:
+Tasks are not super-descriptive this time, so that you can figure out some stuff by
+yourself. Pretend you're working on your own �
+PART 1
+1. Create a function 'createImage' which receives 'imgPath' as an input.
+This function returns a promise which creates a new image (use
+document.createElement('img')) and sets the .src attribute to the
+provided image path
+2. When the image is done loading, append it to the DOM element with the
+'images' class, and resolve the promise. The fulfilled value should be the
+image element itself. In case there is an error loading the image (listen for
+the'error' event), reject the promise
+3. If this part is too tricky for you, just watch the first part of the solution
+PART 2
+4. Consume the promise using .then and also add an error handler
+5. After the image has loaded, pause execution for 2 seconds using the 'wait'
+function we created earlier
+6. After the 2 seconds have passed, hide the current image (set display CSS
+property to 'none'), and load a second image (Hint: Use the image element
+returned by the 'createImage' promise to hide the current image. You will
+need a global variable for that �)
+7. After the second image has loaded, pause execution for 2 seconds again
+8. After the 2 seconds have passed, hide the current image
+Test data: Images in the img folder. Test the error handler by passing a wrong
+image path. Set the network speed to “Fast 3G” in the dev tools Network tab,
+otherwise images load too fast
+GOOD LUCK �
+*/
+
+// This function will wait for seconds
+
+/*
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+let currentImg;
+createImage("img/img-1.jpg")
+  .then((img) => {
+    console.log("Image 1 loaded");
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-2.jpg");
+  })
+  .then((img) => {
+    console.log("Image 2 loaded");
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-3.jpg");
+  })
+  .then((img) => {
+    console.log("Image 3 loaded");
+    currentImg = img;
+    return wait(2);
+  })
+  .then(() => {
+    console.log("All disappear");
+    currentImg.style.display = "none";
+  })
+  .catch((err) => console.error(err));
+*/
